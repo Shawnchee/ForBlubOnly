bbfa78f4fd1ea57f3867aa65f40ab0b49ee6e03e
Shawnchee <aykaychee2@gmail.com>
2023-12-02 00:17:42 +0800

    Finalized first fragment

--- app/src/main/java/com/example/mad_forum/MainActivity.java
+++ app/src/main/java/com/example/mad_forum/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.mad_forum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_most_recent);
+        ImageButton bookmark = (ImageButton) findViewById(R.id.bookmark);
+        bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bookmark.setImageResource(R.drawable.bookmarkpressed);
+            }
+        });
     }
+
 }
